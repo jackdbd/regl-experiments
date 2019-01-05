ce431b54263dbca29827063e9d0c801f563e4491
Giacomo Debidda <jackdebidda@gmail.com>
2019-01-05 13:15:40 +0100

    Updates

---
+++ precache-manifest.c9241980f1db6c658507a0a155ea5a52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7595fea1e3f549a156fb6c363b88dd59",
+    "url": "/regl-experiments/static/media/TypeScriptLogo.7595fea1.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/regl-experiments/static/media/ReactLogo.5d5d9eef.svg"
+  },
+  {
+    "revision": "143b5edea7b2e3d4efd6",
+    "url": "/regl-experiments/static/js/runtime~main.143b5ede.js"
+  },
+  {
+    "revision": "eec5e299977dc253f086",
+    "url": "/regl-experiments/static/js/main.eec5e299.chunk.js"
+  },
+  {
+    "revision": "628a2985f13748babc6f",
+    "url": "/regl-experiments/static/js/2.628a2985.chunk.js"
+  },
+  {
+    "revision": "eec5e299977dc253f086",
+    "url": "/regl-experiments/static/css/main.b24adcb2.chunk.css"
+  },
+  {
+    "revision": "e0498de94da895d95ef2b57a619d9588",
+    "url": "/regl-experiments/index.html"
+  }
+];
